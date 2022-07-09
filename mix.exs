4dda4be519dc462f8ba07b524324d966a6392f2e
Tao Bojlén <66130243+taobojlen@users.noreply.github.com>
2022-07-09 17:42:52 +0100

    extract seed data to its own mix task

--- mix.exs
+++ mix.exs
@@ -97,7 +97,8 @@ defmodule Shroud.MixProject do
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup"],
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.setup": ["ecto.create", "ecto.migrate"],
+      "ecto.seed": ["run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.deploy": [
