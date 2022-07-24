ef4731d6add99e815f8b7f3e41bf9b54d691eb8c
Tao Bojlén <66130243+taobojlen@users.noreply.github.com>
2022-07-24 09:41:54 +0100

    fix reactivity of CopyToClipboardButton

--- lib/shroud_web/components/copy_to_clipboard_button.ex
+++ lib/shroud_web/components/copy_to_clipboard_button.ex
@@ -3,13 +3,16 @@ defmodule ShroudWeb.Components.CopyToClipboardButton do
 
   prop text, :string, required: true
   prop class, :css_class, required: false
+  data alpine_copy_function, :string
 
   def render(assigns) do
-    alpine_copy_function = """
-      navigator.clipboard.writeText('#{assigns.text}');
-      tooltip = 'Copied!';
-      setTimeout(() => tooltip = 'Copy to clipboard', 1500);
-    """
+    assigns =
+      assigns
+      |> assign(:alpine_copy_function, """
+        navigator.clipboard.writeText('#{assigns.text}');
+        tooltip = 'Copied!';
+        setTimeout(() => tooltip = 'Copy to clipboard', 1500);
+      """)
 
     ~F"""
     <div x-data="{ tooltip: 'Copy to clipboard' }" class={@class}>
@@ -17,7 +20,7 @@ defmodule ShroudWeb.Components.CopyToClipboardButton do
         x-tooltip="tooltip"
         type="button"
         class="rounded p-1 focus:ring focus:ring-indigo-500"
-        @click={alpine_copy_function}
+        x-on:click={@alpine_copy_function}
       >
         <Heroicons.Solid.ClipboardCopyIcon class="h-5 w-5" />
       </button>
