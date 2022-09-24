234c2bc131fff4a51280f87d8100c876785cb6bd
Tao Bojlén <66130243+taobojlen@users.noreply.github.com>
2022-09-24 23:23:35 +0100

    only show top bar after 120ms

--- assets/js/app.js
+++ assets/js/app.js
@@ -48,8 +48,17 @@ let liveSocket = new LiveSocket("/live", Socket, {
 
 // Show progress bar on live navigation and form submits
 topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
-window.addEventListener("phx:page-loading-start", (info) => topbar.show())
-window.addEventListener("phx:page-loading-stop", (info) => topbar.hide())
+let topBarScheduled = undefined;
+window.addEventListener("phx:page-loading-start", () => {
+  if(!topBarScheduled) {
+    topBarScheduled = setTimeout(() => topbar.show(), 120);
+  };
+});
+window.addEventListener("phx:page-loading-stop", () => {
+  clearTimeout(topBarScheduled);
+  topBarScheduled = undefined;
+  topbar.hide();
+});
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
