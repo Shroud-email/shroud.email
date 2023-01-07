110600c53e8534edebdae4bd8eb49b0113e1b8c3
Tao Bojlén <66130243+taobojlen@users.noreply.github.com>
2023-01-07 16:47:57 +0000

    update dependencies

--- mix.exs
+++ mix.exs
@@ -42,7 +42,7 @@ defmodule Shroud.MixProject do
       {:bcrypt_elixir, "~> 3.0"},
       {:phoenix, "~> 1.6.2"},
       {:phoenix_ecto, "~> 4.4"},
-      {:ecto_sql, "~> 3.6"},
+      {:ecto_sql, "~> 3.9"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
