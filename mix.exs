406346b82d4212e0f1a73caa90c73a52ad3928d9
Tao Bojlén <66130243+taobojlen@users.noreply.github.com>
2023-03-11 15:07:34 +0000

    chore(main): release 0.2.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Shroud.MixProject do
   def project do
     [
       app: :shroud,
-      version: "0.2.3",
+      version: "0.2.4",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
