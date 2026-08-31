6f92a2bbc6d3747eaaabdea2acf751a716d22d18
Tao Bojlén <git@btao.org>
2026-08-31 14:57:09 +0100

    chore: optimize setup for Amp orbs (#175)
    * chore: optimize setup for Amp orbs
    
    * chore: seed new Amp orbs
    
    * fix: make development seeding atomic
    
    ---------
    
    Co-authored-by: Tao Bojlén <tao@workinghypothes.is>

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -16,80 +16,85 @@ alias Shroud.Repo
 alias Shroud.Domain
 alias Shroud.Email
 
-user =
-  %User{}
-  |> User.registration_changeset(%{
-    email: "test@test.com",
-    password: "123456789012",
-    status: :active
-  })
-  |> Repo.insert!(returning: true)
+{:ok, _user} =
+  Repo.transact(fn ->
+    user =
+      %User{}
+      |> User.registration_changeset(%{
+        email: "test@test.com",
+        password: "123456789012",
+        status: :active
+      })
+      |> Repo.insert!(returning: true)
 
-user
-|> User.confirm_changeset(%{status: :active})
-|> Repo.update!()
+    user
+    |> User.confirm_changeset(%{status: :active})
+    |> Repo.update!()
 
-now = NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)
+    now = NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)
 
-{:ok, domain} = Domain.create_custom_domain(user, %{domain: "verified.com"})
+    {:ok, domain} = Domain.create_custom_domain(user, %{domain: "verified.com"})
 
-domain
-|> Ecto.Changeset.change(%{
-  ownership_verified_at: now,
-  mx_verified_at: now,
-  spf_verified_at: now,
-  dkim_verified_at: now,
-  dmarc_verified_at: now
-})
-|> Repo.update!()
+    domain
+    |> Ecto.Changeset.change(%{
+      ownership_verified_at: now,
+      mx_verified_at: now,
+      spf_verified_at: now,
+      dkim_verified_at: now,
+      dmarc_verified_at: now
+    })
+    |> Repo.update!()
 
-user
-|> Domain.create_custom_domain(%{domain: "unverified.com"})
+    {:ok, _domain} = Domain.create_custom_domain(user, %{domain: "unverified.com"})
 
-aliases = [
-  %{title: "e-shop.co"},
-  %{title: "1337 Newsletter", enabled: false},
-  %{description: "Lorem ipsum"},
-  %{title: "Anonymous contact address"},
-  %{
-    enabled: false,
-    description: "Haxx0r ipsum brute force server continue void snarf case it's a feature."
-  },
-  %{title: "Twitter"},
-  %{title: ""}
-]
+    aliases = [
+      %{title: "e-shop.co"},
+      %{title: "1337 Newsletter", enabled: false},
+      %{description: "Lorem ipsum"},
+      %{title: "Anonymous contact address"},
+      %{
+        enabled: false,
+        description: "Haxx0r ipsum brute force server continue void snarf case it's a feature."
+      },
+      %{title: "Twitter"},
+      %{title: ""}
+    ]
 
-Enum.each(aliases, fn alias_attrs ->
-  Aliases.create_random_email_alias(user, alias_attrs)
+    Enum.each(aliases, fn alias_attrs ->
+      {:ok, _email_alias} = Aliases.create_random_email_alias(user, alias_attrs)
 
-  # Sleep to ensure that the aliases are created in the correct order (i.e. not at the exact same timestamp)
-  :timer.sleep(500)
-end)
+      # Sleep to ensure that the aliases are created in the correct order (i.e. not at the exact same timestamp)
+      :timer.sleep(500)
+    end)
 
-Aliases.create_email_alias(%{
-  user_id: user.id,
-  title: "Airport WiFi",
-  description: "Created via catch-all",
-  address: "airport@verified.com"
-})
+    {:ok, _email_alias} =
+      Aliases.create_email_alias(%{
+        user_id: user.id,
+        title: "Airport WiFi",
+        description: "Created via catch-all",
+        address: "airport@verified.com"
+      })
 
-user
-|> Aliases.list_aliases()
-|> Enum.each(fn email_alias ->
-  number_forwarded = :rand.uniform(20)
-  number_blocked = :rand.uniform(5)
+    user
+    |> Aliases.list_aliases()
+    |> Enum.each(fn email_alias ->
+      number_forwarded = :rand.uniform(20)
+      number_blocked = :rand.uniform(5)
 
-  Enum.each(1..number_forwarded, fn _ ->
-    Aliases.increment_forwarded!(email_alias)
+      Enum.each(1..number_forwarded, fn _ ->
+        Aliases.increment_forwarded!(email_alias)
+      end)
+
+      Enum.each(1..number_blocked, fn _ ->
+        Aliases.increment_blocked!(email_alias)
+      end)
+
+      Email.store_spam_email!(
+        %{text_body: "Lorem ipsum", from: "spammer@test.com", subject: "Spam"},
+        user,
+        email_alias
+      )
+    end)
+
+    {:ok, user}
   end)
-
-  Enum.each(1..number_blocked, fn _ ->
-    Aliases.increment_blocked!(email_alias)
-  end)
-
-  Email.store_spam_email!(
-    %{text_body: "Lorem ipsum", from: "spammer@test.com", subject: "Spam"},
-    user,
-    email_alias
-  )
-end)
